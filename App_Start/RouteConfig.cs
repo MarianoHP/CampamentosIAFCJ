015e5e183e90a40bbd9c11bbcb4c241622977d78
MarianoHP <47233997+MarianoHP@users.noreply.github.com>
2019-05-15 23:18:32 -0700

    Version 1.7
    Version casi completa

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CampIAFCJ
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Validar", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
